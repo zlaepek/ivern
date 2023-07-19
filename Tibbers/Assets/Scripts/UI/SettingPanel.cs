7269080bf802233d89b076451a662eb97abdcca1
zlaepek <alice4813@naver.com>
2023-07-19 17:42:22 +0900

    MODIFY Setting Panel Position

--- Tibbers/Assets/Scripts/UI/SettingPanel.cs
+++ Tibbers/Assets/Scripts/UI/SettingPanel.cs
@@ -8,23 +8,97 @@ using UnityEngine.UI;
 public class SettingPanel : MonoBehaviour
 {
     #region VARIABLES
+    
+    public enum SettingState
+    {
+        Audio,
+        Language,
+        AppInfo
+    }
+
+    [Header("SettingPanel")]
+    public GameObject settingPanel;
+
+    [Header("OptionPanel")]
+    public SettingState currentSettingState;
+
+    public GameObject audioSettingPanel;
+    public GameObject languageSettingPanel;
+    public GameObject appInfoSettingPanel;
+
+    [Header("Audio")]
     public Slider sfxSlider;
     public Slider bgmSlider;
 
+    [Header("Language")]
     public TMP_Dropdown langunageDropdown;
     #endregion
 
-    // Start is called before the first frame update
+    #region LifeCycle
     void Start()
     {
+        // 언어 설정 불러오기
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[GetLocalePref()];
     }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    #region Initialize
+    public void SettingPanelOn()
     {
-        
+        settingPanel.SetActive(true);
+        SettingPanelSelect();
     }
+    public void SettingPanelOff()
+    {
+        settingPanel?.SetActive(false);
+        currentSettingState = SettingState.Audio;
+    }
+    #endregion
+
+    #region Panel Control
+    private void AllSettingPanelOff()
+    {
+        audioSettingPanel.SetActive(false);
+        languageSettingPanel.SetActive(false);
+        appInfoSettingPanel.SetActive(false);
+    }
+
+    private void SettingPanelSelect()
+    {
+        AllSettingPanelOff();
+
+        switch (currentSettingState)
+        {
+            case SettingState.Audio:
+                audioSettingPanel.SetActive(true);
+                break;
+            case SettingState.Language:
+                languageSettingPanel.SetActive(true);
+                break;
+            case SettingState.AppInfo:
+                appInfoSettingPanel.SetActive(true);
+                break;
+            default: 
+                break;
+        }
+    }
+
+    public void AudioPanelSelected()
+    {
+        currentSettingState = SettingState.Audio;
+        SettingPanelSelect();
+    }
+    public void LanguagePanelSelected()
+    {
+        currentSettingState = SettingState.Language;
+        SettingPanelSelect();
+    }
+    public void AppInfoPanelSelected()
+    {
+        currentSettingState = SettingState.AppInfo;
+        SettingPanelSelect();
+    }
+    #endregion
 
     #region Audio
     public void SetVolumeSFX()
