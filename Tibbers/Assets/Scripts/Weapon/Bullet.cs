3d78b9fe8ec2b6f2fd9be0a593b67db4e69cddce
Hexanon9 <thuey@naver.com>
2023-09-09 22:39:58 +0900

    Bullet, Weapon, Player
     - Fix to Player Move (velocity)
     - Add Structs
     - Add Bullet
     - Add Weapon

---
+++ Tibbers/Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Structs;
+
+public class Bullet : MonoBehaviour
+{
+    public Structs.BulletStat m_stStat;
+    //float m_fBulletDamage;
+    //float m_fKnockbackForce;
+    //float fMoveSpeed;
+
+    private GameObject m_Master;
+    private Rigidbody2D m_RigidBody;
+    private Vector2 m_vDir;
+
+    private Animator m_Animator;
+    private float LifeTime;
+
+    public float m_fKnockbackForce { get { return m_stStat.fKnockbackForce;  } set { m_stStat.fKnockbackForce = value; } }
+    public float m_fMoveSpeed { get { return m_stStat.fMoveSpeed; } set { m_stStat.fMoveSpeed = value; } }
+    public float m_fBulletDamage { get { return m_stStat.fBulletDamage; } set { m_stStat.fBulletDamage = value; } }
+    private void RotateBullet()
+    {
+        float fAngle = Mathf.Atan2(m_vDir.y, m_vDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(fAngle, Vector3.forward);
+    }
+    private void MoveBullet()
+    {
+        m_RigidBody.velocity = m_vDir * m_stStat.fMoveSpeed;
+
+    }
+    //private void FlipX()
+    //{
+    //    Vector3 vScale = transform.localScale;
+    //    vScale.x *= -1.0f;
+    //    transform.localScale = vScale;
+    //}
+
+    #region LifeCycle
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+
+        m_Animator.SetTrigger("Create");
+
+        m_stStat = default;
+        m_fKnockbackForce = 10.0f;
+        m_fMoveSpeed = 5.0f;
+        m_fBulletDamage = 5.0f;
+
+        LifeTime = 3.0f;
+
+        Destroy(gameObject, LifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBullet();
+    }
+
+#endregion
+
+#region Collision
+    private void OnTriggerEnter2D(Collider2D _Collision)
+    {
+        _Collision.GetComponent<Unit>().GetDamage(m_stStat.fBulletDamage, m_stStat.fKnockbackForce, m_vDir);
+
+        Destroy(gameObject);
+    }
+    #endregion
+
+#region Setter
+    public void SetDir(Vector2 _Vec)
+    {
+        m_vDir = _Vec;
+    }
+
+    public void SetMaster(GameObject _Master)
+    {
+        m_Master = _Master;
+    }
+
+    //public float fBulletDamage;
+    //public float fKnockbackForce;
+    //public float fMoveSpeed;
+   
+
+#endregion
+}
---
+++ Tibbers/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Structs;
+
+public class Weapon
+{
+    public GameObject m_BulletPrefab;
+
+    private Structs.WeaponStat m_stStat;
+    private float m_fNextAttack;
+    private float m_fNextDelay;
+    private Quaternion m_BulletRotation;
+    private Vector3 m_vBulletPos;
+    private GameObject m_Master;
+
+    private BulletManager.eBulletType m_eType;
+     //Start is called before the first frame update
+    public Weapon()
+    {
+        m_fNextAttack = Time.time;
+
+    }
+
+
+    public void Attack(Vector3 _vShotterPos, Vector2 _vAttackDir, float _fBaseAttackSpeed)
+    {
+        
+        // 일정 주기마다 발사하기
+        if (Time.time > m_fNextAttack)
+        {
+            // 공격속도 = 기본공격속도 * ( 1 + 장비보너스 / 100 ) * ( 1 + 능력치보너스 / 100 )
+            float fTotalSpeed = m_stStat.fAttackSpeed * (1 + (_fBaseAttackSpeed / 100));
+            float fAttackInterval = 1 / fTotalSpeed;
+
+            m_fNextAttack = Time.time + fAttackInterval;
+
+
+            switch (m_eType)
+            {
+                case BulletManager.eBulletType.BulletType_EnergyBall:
+                    {
+                        RotateBullet(_vAttackDir);
+                        CreateAttackPos(_vShotterPos, _vAttackDir);
+
+                        for (int i = 0; i < m_stStat.iAttackCount; ++i)
+                        {
+                            if (Time.time > m_fNextDelay)
+                            {
+                                m_fNextDelay = Time.time + m_stStat.fAttackDelay;
+
+                                GameObject tempBullet = GameObject.Instantiate(m_BulletPrefab, m_vBulletPos, m_BulletRotation);
+                                tempBullet.GetComponent<Bullet>().SetDir(_vAttackDir);
+                            }
+                        }
+                    }
+                    break;
+
+                case BulletManager.eBulletType.BulletType_Melee:
+                    {
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void RotateBullet(Vector2 _vAttackDir)
+    {
+        float fAngle = Mathf.Atan2(_vAttackDir.y, _vAttackDir.x) * Mathf.Rad2Deg;
+        m_BulletRotation = Quaternion.AngleAxis(fAngle, Vector3.forward);
+    }
+    private void CreateAttackPos(Vector3 _vShotterPos, Vector2 _vAttackDir)
+    {
+        m_vBulletPos = _vShotterPos + (Vector3)(_vAttackDir * m_stStat.fAttackRange);
+    }
+
+    #region Setter
+    public int SetAttackCount { set { m_stStat.iAttackCount = value; } }
+    public float SetAttackSpeed { set { m_stStat.fAttackSpeed = value; } }
+    public float SetAttackRange { set { m_stStat.fAttackRange = value; } }
+    public float SetAttackDelay { set { m_stStat.fAttackDelay = value; } }
+    public float SetBulletSpeed { set { m_BulletPrefab.GetComponent<Bullet>().m_fMoveSpeed = value; } }
+    public float SetBulletDamage { set { m_BulletPrefab.GetComponent<Bullet>().m_fBulletDamage = value; } }
+    public float SetBulletKnockback { set { m_BulletPrefab.GetComponent<Bullet>().m_fKnockbackForce = value; } }
+    public void SetMaster(GameObject _Master) { m_Master = _Master; }
+    public void SetBullet(GameObject _BulletPrefab, BulletManager.eBulletType _eType) 
+    { 
+        m_BulletPrefab = _BulletPrefab;
+        m_eType = _eType;
+    }
+    #endregion
+}
