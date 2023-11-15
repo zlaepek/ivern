d2bc3d50a03a6c1bc744724a3ce49c14bfd32cf3
zlaepek <69634389+zlaepek@users.noreply.github.com>
2023-11-15 23:50:58 +0900

    stage 씬 오류 수정

--- Tibbers/Assets/Scripts/Gacha/Gacha.cs
+++ Tibbers/Assets/Scripts/Gacha/Gacha.cs
@@ -7,7 +7,21 @@ public class Gacha : MonoBehaviour
 {
     public GameObject gachaResultPrefab = null;
 
-    public IObjectPool<GameObject> GachaResultPool { get; set; }
+    private IObjectPool<GameObject> _gachaResultPool;
+    //public IObjectPool<GameObject> GachaResultPool
+    //{
+    //    get
+    //    {
+    //        if (_gachaResultPool == null)
+    //        {
+    //            if (poolType == PoolType.Stack)
+    //                _gachaResultPool = new ObjectPool<ParticleSystem>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, collectionChecks, 10, maxPoolSize);
+    //            else
+    //                _gachaResultPool = new LinkedPool<ParticleSystem>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, collectionChecks, maxPoolSize);
+    //        }
+    //        return _gachaResultPool;
+    //    }
+    //}
 
     public GameObject resultPrefab = null;
     public void ShowGachaListResult(List<GachaJson> gachaResults)
