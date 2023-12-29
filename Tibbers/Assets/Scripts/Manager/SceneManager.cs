df864a6eb56219d3592382af79faf98c8829e875
zlaepek <69634389+zlaepek@users.noreply.github.com>
2023-12-29 17:30:02 +0900

    씬 매니저 수정

--- Tibbers/Assets/Scripts/Manager/SceneManager.cs
+++ Tibbers/Assets/Scripts/Manager/SceneManager.cs
@@ -1,30 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
 
 public class SceneManager : MonoBehaviour
 {
-    public string PanelName;
-
-    public void OpenPanel()
+    #region Static SceneManager
+    private static SceneManager _instance = null;
+    public static SceneManager Instance
     {
-        // SceneManager.LoadScene(PanelName, LoadSceneMode.Additive);
+        get
+        {
+            return _instance;
+
+        }
+    }
+    #endregion
+
+    #region Life Cycle (Initialize)
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            _CommonScene();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+    private void _SceneChange(string _strSceneName)
+    {
+        UnitySceneManager.LoadScene(_strSceneName);
     }
 
-    private void SceneChange(string _strSceneName)
+    private void _SceneAdd(string _strSceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(_strSceneName);
+        UnitySceneManager.LoadScene(_strSceneName, LoadSceneMode.Additive);
+    }
+    private AsyncOperation _LoadSceneAsync(string _strSceneName)
+    {
+        return UnitySceneManager.LoadSceneAsync(_strSceneName);
+    }
+
+    #region Each Scene Load Method
+    private void _CommonScene()
+    {
+        _SceneAdd("Scene_Common");
     }
 
     public void SceneChange_Title()
     {
-        SceneChange("Scene_Title");
+        _SceneChange("Scene_Title");
     }
 
     public void SceneChange_Stage()
     {
-        SceneChange("Scene_Stage");
+        _SceneChange("Scene_Stage");
         NetworkManager.Instance?.RequestPostChapterStart(1);
     }
+    #endregion
 }
